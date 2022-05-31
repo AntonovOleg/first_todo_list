bb73c69ca52bd7adae1fe9de806bb2af9f6fa0c4
Oleg <oleg.ant.d@mail.ru>
2022-05-31 13:58:55 +0300

    pre-worked todo

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function task(dateOfCreate, dateOfOut, caption, status_flag){
 
 let todos = [] //массив заданий
 
-function refresh_list(){
+function refresh_list(filter_mode){
     //обновление дом в соответствии с данными в массиве 
 
     let todos_dom = document.getElementsByClassName('list_todos')[0];
@@ -36,33 +36,95 @@ function refresh_list(){
 
     //рисуем все задания
     for (i in todos){
-        let obj = document.createElement('li');
-        obj.classList.add('task');
-        let text = document.createTextNode(todos[i].dateOfCreate + todos[i].dateOfOut + todos[i].caption + todos[i].status_flag);
-        obj.appendChild(text);
+        if(filter_mode==0){
+            let obj = document.createElement('li');
+            obj.classList.add('task');
+            let text = document.createTextNode(todos[i].dateOfCreate +'| ' + todos[i].dateOfOut +'| '+ todos[i].caption + '| '+todos[i].status_flag);
+            obj.appendChild(text);
 
-        //obj.innerHTML+='<button class="inProgress">Приступить</button><button class="complete">Выполнено</button><button class="delete">Удалить</button>'
+            //obj.innerHTML+='<button class="inProgress">Приступить</button><button class="complete">Выполнено</button><button class="delete">Удалить</button>'
 
-        let btn1 = document.createElement('button');
-        btn1.classList.add('inProgress');
-        btn1.innerText='Приступить';
-        btn1.onclick=setInProgress;
+            let btn1 = document.createElement('button');
+            btn1.classList.add('inProgress');
+            btn1.innerText='Приступить';
+            btn1.onclick=setInProgress;
 
-        let btn2 = document.createElement('button');
-        btn2.classList.add('complete');
-        btn2.innerText='Выполнено';
-        btn2.onclick=setComplete;
+            let btn2 = document.createElement('button');
+            btn2.classList.add('complete');
+            btn2.innerText='Выполнено';
+            btn2.onclick=setComplete;
 
-        let btn3 = document.createElement('button');
-        btn3.classList.add('delete');
-        btn3.innerText='Удалить';
-        btn3.onclick=delete_item;
+            let btn3 = document.createElement('button');
+            btn3.classList.add('delete');
+            btn3.innerText='Удалить';
+            btn3.onclick=delete_item;
 
-        obj.appendChild(btn1);
-        obj.appendChild(btn2);
-        obj.appendChild(btn3);
+            obj.appendChild(btn1);
+            obj.appendChild(btn2);
+            obj.appendChild(btn3);
 
-        todos_dom.appendChild(obj);
+            todos_dom.appendChild(obj);
+        } else if(filter_mode==1){
+            if(todos[i].status_flag=='В процессе'){
+                let obj = document.createElement('li');
+                obj.classList.add('task');
+                let text = document.createTextNode(todos[i].dateOfCreate +'| ' + todos[i].dateOfOut +'| '+ todos[i].caption + '| '+todos[i].status_flag);
+                obj.appendChild(text);
+
+                //obj.innerHTML+='<button class="inProgress">Приступить</button><button class="complete">Выполнено</button><button class="delete">Удалить</button>'
+
+                let btn1 = document.createElement('button');
+                btn1.classList.add('inProgress');
+                btn1.innerText='Приступить';
+                btn1.onclick=setInProgress;
+
+                let btn2 = document.createElement('button');
+                btn2.classList.add('complete');
+                btn2.innerText='Выполнено';
+                btn2.onclick=setComplete;
+
+                let btn3 = document.createElement('button');
+                btn3.classList.add('delete');
+                btn3.innerText='Удалить';
+                btn3.onclick=delete_item;
+
+                obj.appendChild(btn1);
+                obj.appendChild(btn2);
+                obj.appendChild(btn3);
+
+                todos_dom.appendChild(obj);
+            }
+        } else if(filter_mode==2){
+            if(todos[i].status_flag=='Выполнено'){
+                let obj = document.createElement('li');
+                obj.classList.add('task');
+                let text = document.createTextNode(todos[i].dateOfCreate +'| ' + todos[i].dateOfOut +'| '+ todos[i].caption + '| '+todos[i].status_flag);
+                obj.appendChild(text);
+
+                //obj.innerHTML+='<button class="inProgress">Приступить</button><button class="complete">Выполнено</button><button class="delete">Удалить</button>'
+
+                let btn1 = document.createElement('button');
+                btn1.classList.add('inProgress');
+                btn1.innerText='Приступить';
+                btn1.onclick=setInProgress;
+
+                let btn2 = document.createElement('button');
+                btn2.classList.add('complete');
+                btn2.innerText='Выполнено';
+                btn2.onclick=setComplete;
+
+                let btn3 = document.createElement('button');
+                btn3.classList.add('delete');
+                btn3.innerText='Удалить';
+                btn3.onclick=delete_item;
+
+                obj.appendChild(btn1);
+                obj.appendChild(btn2);
+                obj.appendChild(btn3);
+
+                todos_dom.appendChild(obj);
+            }
+        }
     }
 
 }
@@ -77,21 +139,92 @@ function add_task(){
     let newTask = new task(dateOfCreate,dateOfOut,caption,status_flag);
     todos.push(newTask);
 
-    refresh();
+    refresh(0);
 }
 
-function refresh(){
-    refresh_list();
+function refresh(filter_mode){
+    refresh_list(filter_mode);
 }
 
 function setInProgress(){
-    console.log(this);
+    //проверяем какой это таск по контексту
+    let context = this;
+    let num = getNumberTask(context,0);
+
+    //вносим изменения и обновляем
+    todos[num].status_flag = 'В процессе';
+    refresh(0);
 }
 
 function setComplete(){
-    console.log(this);
+    //проверяем какой это таск по контексту
+    let context = this;
+    let num = getNumberTask(context,1);
+
+    //вносим изменения и обновляем
+    todos[num].status_flag = 'Выполнено';
+    refresh(0);
 }
 
 function delete_item(){
+    let todos_dom = document.getElementsByClassName('list_todos')[0];
+
+    //проверяем какой это таск по контексту
+    let context = this;
+
+    console.log('This = ');
     console.log(this);
+
+    let num = getNumberTask(context,2);
+
+    console.log('number of context = ');
+    console.log(num);
+
+    //вносим изменения и обновляем
+
+    console.log('num is ' + num);
+    console.log('testing');
+    console.log(todos_dom);
+    console.log(todos_dom.getElementsByTagName('li')[num]);
+    
+    todos.splice(num,1);
+    todos_dom.removeChild(todos_dom.getElementsByTagName('li')[num]);
+
+    refresh(0);
+}
+
+function getNumberTask(context, typeBtn){
+    let todos_dom = document.getElementsByClassName('list_todos')[0];
+    for(let j=todos_dom.getElementsByTagName('li').length-1;j>=0;j--){
+        if(todos_dom.getElementsByTagName('li')[j].getElementsByTagName('button')[parseInt(typeBtn)] == context){
+            return j;
+        }    
+    }
+}
+
+function button_see_all(){
+    refresh(0);
+}
+
+function button_see_inprogress(){
+    refresh(1);
+}
+
+function button_see_complete(){
+    refresh(2);
+}
+
+function delete_all(){
+    let todos_dom = document.getElementsByClassName('list_todos')[0];
+    if(prompt('del all y/n')=='y'){
+        //удаление всех заданий и обновление рендера
+        console.log('del all');
+
+        for(let i=todos_dom.getElementsByTagName('li').length-1;i>=0;i--){
+            console.log(i);
+            todos_dom.removeChild(todos_dom.getElementsByTagName('li')[i]);
+        }
+
+        todos.splice(0,todos.length);
+    }
 }
